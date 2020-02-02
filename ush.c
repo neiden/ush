ba4fc58db309c9c5202a749ae799ad7254cc6c15
swanbej3 <swanbej3@wwu.edu>
2020-02-02 02:50:31 +0000

    Update ush.c

--- ush.c
+++ ush.c
@@ -11,6 +11,7 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <ctype.h>
+#include "argparse.h"
 
 
 /* CONSTANTS */
@@ -23,56 +24,6 @@
  * new process that executes that command.
  */
 void processline (char *line);
-int numElements(char* line);
-char** argparse(char* line, int* argcp);
-
-int numElements(char* line){
-    enum {BET, ARG} state = BET;
-    int count = 0;
-
-    while(*line != '\0'){
-        if(!isspace(*line)){
-            if(state == BET){
-                ++count;
-            }
-            state = ARG;
-        }
-        else {
-            state = BET;
-        }
-        ++line;
-        }
-    return count;
-    }
-
-char** argparse(char* line, int* argcp){
-        int elements = numElements(line);
-        *argcp = elements;
-        int count = 0;
-
-        char** args = (char**)malloc((elements + 1)*sizeof(char*));
-        enum {BET, ARG} state = BET;
-
-        while(*line != '\0') {
-
-            if(!isspace(*line)) {
-                if(state == BET){
-                    args[count] = line;
-                    ++count;
-                }
-                state = ARG;
-            }
-            else {
-                if(state == ARG){
-                    *line = '\0';
-                }
-                state = BET;
-            }
-            ++line;
-        }
-
-        return args;
-    }
 
 
 
