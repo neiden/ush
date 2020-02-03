4183d958543c2f0457c30afbd4fbc208dd428d89
swanbej3 <swanbej3@wwu.edu>
2020-02-03 06:08:25 +0000

    Update argparse.c

--- argparse.c
+++ argparse.c
@@ -3,23 +3,42 @@
 //
 #include <stdlib.h>
 #include <ctype.h>
+#include <stdio.h>
 #include "argparse.h"
 
 static int numElements(char* line){
-    enum {BET, ARG} state = BET;
+    enum {BET, ARG, QUOTE} state = BET;
     int count = 0;
-
-    while(*line != '\0'){
+    while(*line != NULL){
         if(!isspace(*line)){
             if(state == BET){
                 ++count;
+                state = ARG;
             }
-            state = ARG;
+            if(state == ARG){
+                if(*line == 34){
+                    state = QUOTE;
+                }
+            }
+            else{
+                if(*line == 34){
+                    state = ARG;
+                }
+            }
+
         }
-        else {
-            state = BET;
+        else{
+            if(state == ARG){
+                state = BET;
+            }
         }
+
         ++line;
+    }
+
+
+    if(state == QUOTE){
+        fprintf(stderr, "Unclosed quotation marks");
     }
     return count;
 }
@@ -30,22 +49,44 @@ char** argparse(char* line, int* argcp){
     int count = 0;
 
     char** args = (char**)malloc((elements + 1)*sizeof(char*));
-    enum {BET, ARG} state = BET;
+    enum {BET, ARG, QUOTE} state = BET;
 
-    while(*line != '\0') {
 
-        if(!isspace(*line)) {
+    while(*line != NULL){
+        if(!isspace(*line)){
             if(state == BET){
-                args[count] = line;
-                ++count;
+                if(*line != 34){
+                    args[count] = line;
+                    ++count;
+                }
+                state = ARG;
             }
-            state = ARG;
-        }
-        else {
             if(state == ARG){
-                *line = '\0';
+                if(*line == 34){
+                    if(line[1] == 34){
+                        args[count] = NULL;
+                    }
+                    else{
+                        ++line;
+                        args[count] = line;
+
+                    }
+                    ++count;
+                    state = QUOTE;
+                }
             }
-            state = BET;
+            else{
+                if(*line == 34){
+                    *line = NULL;
+                    state = ARG;
+                }
+            }
+        }
+        else{
+            if(state == ARG){
+                *line = NULL;
+                state = BET;
+            }
         }
         ++line;
     }
